79845a4897c08efbd1243600d3f2793b0ae4cb20
Marco <marco.sanjuan@gmail.com>
2025-08-25 14:14:34 +0200

    Init

---
+++ src/main/java/com/ing/springeventstalk/SpringEventsTalkApplication.java
@@ -0,0 +1,13 @@
+package com.ing.springeventstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventsTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventsTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ing/springeventstalk/SpringEventsTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.springeventstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventsTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
